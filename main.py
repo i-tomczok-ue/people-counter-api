4979af4c3dcd2849e7aa3bd010f195f5a1f811e4
i-tomczok-ue <iwona.tomczok@edu.uekat.pl>
2024-01-28 16:06:57 +0100

    API with three endpoints

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+import flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 import cv2
+import requests
+from io import BytesIO
+import numpy as np
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,13 +20,42 @@ class PeopleCounter(Resource):
         return {'count': len(boxes)}
 
 
+
+class PeopleCounterWithLinkGiven(Resource):
+    def get(self):
+        url = request.args.get('url')
+        if url:
+            img2 = cv2.imread(url)
+            boxes, weights = hog.detectMultiScale(img2, winStride=(5, 5))
+            return {'count from link': len(boxes)}
+        else:
+            return {'error': 'url parameter not provided'}
+
+
+class PeopleCounterWithImage(Resource):
+    def post(self):
+        if 'image' not in request.files:
+            return {'error': 'No image provided'}
+        img_file = request.files['image']
+        if img_file.filename == '':
+            return {'error': 'No image filename provided'}
+        if img_file:
+            img_data = img_file.read()
+            img = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_COLOR)
+            boxes, weights = hog.detectMultiScale(img, winStride=(5, 5))
+            return {'count from image': len(boxes)}
+
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 
+api.add_resource(PeopleCounterWithLinkGiven, '/link')
 api.add_resource(PeopleCounter, '/')
+api.add_resource(PeopleCounterWithImage, '/image')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
